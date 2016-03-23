3277fe6c482f1fb81ec562417c10091d4d8521de
jordiabante <jordi.abante@tamu.edu>
2016-03-23 11:33:47 -0500

    Changed test case

--- src/himme_scoring/main/main.sh
+++ src/himme_scoring/main/main.sh
@@ -5,3 +5,5 @@ rm -rf out
 set -x
 ../himme_scoring.sh -t 1 -k 1 -d out \
     grch37_cdna_tm1.txt.gz emission_test.txt.gz input.fa
+../himme_scoring.sh -t 1 -k 3 -d out \
+    grch37_cdna_tm3.txt.gz emission_test.txt.gz input.fa
