e1d8d88b031065563ec360df7bd512a1945ba165
jordiabante <jordi.abante@tamu.edu>
2016-03-23 12:07:20 -0500

    Resolved conflicts

--- src/himme_scoring/main/main.sh
+++ src/himme_scoring/main/main.sh
@@ -3,10 +3,5 @@
 rm -rf out
 
 set -x
-<<<<<<< HEAD
-../himme_scoring.sh -t 1 -k 3 -d out \
-    grch37_cdna_tm3.txt.gz emission_test.txt.gz input.fa
-=======
 ../himme_scoring.sh -t 3 -k 1 -d out \
     transition_matrix.txt.gz emission_matrix.txt.gz input.fa
->>>>>>> dev
