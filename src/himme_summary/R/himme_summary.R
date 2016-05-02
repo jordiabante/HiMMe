e9d2b4eab83ae51515e65afec12e1a274e44e023
jordiabante <jordi.abante@tamu.edu>
2016-05-02 12:32:09 -0500

    Updated summary

--- src/himme_summary/R/himme_summary.R
+++ src/himme_summary/R/himme_summary.R
@@ -19,6 +19,10 @@ out=rbind(out,c("Median length",x_median_l))
 x_median_s=median(x[,3])
 out=rbind(out,c("Median score",x_median_s))
 
+# Score hat Variance
+x_variance=var(x[,3])
+out=rbind(out,c("Score variance",x_variance))
+
 # 95% CI corrected score
 x_density=density(x[,3])
 x_quantiles=quantile(x_density$x,c(0.05,0.95))
@@ -27,6 +31,10 @@ out=rbind(out,c("Score interval [5%,95%]",paste(x_quantiles[1],",",x_quantiles[2
 # Median score hat
 x_median_shat=median(x[,4])
 out=rbind(out,c("Median corrected score",x_median_shat))
+
+# Score hat Variance
+x_variance=var(x[,4])
+out=rbind(out,c("Score hat variance",x_variance))
 
 # 95% CI corrected score
 x_density=density(x[,4])
