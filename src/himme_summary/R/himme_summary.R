be2d83d86cd7f3f566f1a99d6178e7ab5af90935
jordiabante <jordi.abante@tamu.edu>
2016-07-12 13:19:08 +0200

    Added coefficient to summary output

--- src/himme_summary/R/himme_summary.R
+++ src/himme_summary/R/himme_summary.R
@@ -23,7 +23,7 @@ out=rbind(out,c("Length variance",x_variance))
 x_median_s=median(x[,3])
 out=rbind(out,c("Median score",x_median_s))
 
-# Score hat Variance
+# Score Variance
 x_variance=var(x[,3])
 out=rbind(out,c("Score variance",x_variance))
 
@@ -45,6 +45,9 @@ x_density=density(x[,4])
 x_quantiles=quantile(x_density$x,c(0.05,0.95))
 out=rbind(out,c("Corrected score interval [5%,95%]",paste(x_quantiles[1],",",x_quantiles[2])))
 
+# HiMMe coeefficient
+x_coeff=1/(x_variance*abs(x_median_shat))
+out=rbind(out,c("HiMMe coefficient",paste(x_coeff)))
+
 # Save output
 write.table(out,file=outfile,sep='\t',quote=F,row.names=F,col.names=F)
-
